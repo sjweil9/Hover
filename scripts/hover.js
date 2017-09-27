1ee7245a6777c22639bbcacc3144df0ff5bf833e
StephenWeil <stephen.weil@gmail.com>
2017-09-26 20:49:00 -0500

    changed that horrible code into an actually generalizable solution

--- scripts/hover.js
+++ scripts/hover.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
     $('img').hover(function(){
-        $(this).data('origsrc', $(this).attr('src'));
-        if ($(this).attr('src') == "images/pikachu.jpeg") {
-            $(this).attr('src', 'images/raichu.jpeg');
-        }
-        else if ($(this).attr('src') == "images/charmander.png") {
-            $(this).attr('src', 'images/charizard.png');
-        }
-        else if ($(this).attr('src') == "images/squirtle.png") {
-            $(this).attr('src', 'images/blastoise.png');
-        }
-        else {
-            $(this).attr('src', 'images/venusaur.jpg');
-        }
+        var temp = $(this).attr('src');
+        $(this).attr('src', $(this).data('alt-src'));
+        $(this).data('alt-src', temp);
     }, function() {
-        $(this).attr('src', $(this).data('origsrc'));
+        var temp = $(this).attr('src');
+        $(this).attr('src', $(this).data('alt-src'));
+        $(this).data('alt-src', temp);
     });
 })
